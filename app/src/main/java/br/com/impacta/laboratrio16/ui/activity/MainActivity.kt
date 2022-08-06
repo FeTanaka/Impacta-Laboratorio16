3374f8a65d6b586fe5bbd957643c7418f21f3746
curso <curso@curso.com>
2022-08-05 22:50:34 -0300

    layouts

--- app/src/main/java/br/com/impacta/laboratrio16/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/impacta/laboratrio16/ui/activity/MainActivity.kt
@@ -3,8 +3,14 @@ package br.com.impacta.laboratrio16.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.impacta.laboratrio16.R
+import br.com.impacta.laboratrio16.data.local.database.AppDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    val db: AppDatabase by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
